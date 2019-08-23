75dd0ed9165f54b71736dcb0e819917be040035b
Dmitry16 <europartner@inbox.ru>
2019-08-23 15:24:00 +0300

    types

--- backend/src/graphql-schema/index.js
+++ backend/src/graphql-schema/index.js
@@ -9,17 +9,53 @@ const {
   GraphQLList
 } = require('graphql');
 
+const MovieType = new GraphQLObjectType({
+  name: 'Movie',
+  description: 'a movie object',
+
+  fields: () => ({
+    Title: {
+      type: GraphQLString,
+      resolve: () => 'Title!'
+    },
+    Year: {
+      type: GraphQLString,
+      resolve: () => 'Year!'
+    },
+    Type: {
+      type: GraphQLString,
+      resolve: () => 'Type!'
+    },
+    Poster: {
+      type: GraphQLString,
+      resolve: () => 'Poster!'
+    }
+  })
+});
+
+const SearchType = new GraphQLObjectType({
+  name: 'Search',
+  description: 'Search data array',
+
+  fields: () => ({
+    movie: {
+      type: new GraphQLList(MovieType),
+      resolve: () => 'movies here!'
+    }
+  })
+})
+
 module.exports = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
     description: 'the entry point',
 
     fields: () => ({
-      greeting: {
-        type: GraphQLString,
-        resolve: async (source, args) => {
+      search: {
+        type: SearchType,
+        resolve: async () => {
           const data = await httpClient.fetchOmdbApi.getMovies(axios, 'rock', 1);
-          console.log('dataaaaa:::', data);
+          console.log('dataaaaa:::', data); 
           return data;
         }
       }
